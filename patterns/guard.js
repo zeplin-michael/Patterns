eac670c809eca18767c22bf8096e4d38beef39ab
Zeplin Rowe <rowe_michael101@yahoo.com>
2025-04-30 18:02:23 -0400

    Guard Passed

--- patterns/guard.js
+++ patterns/guard.js
@@ -19,6 +19,13 @@ export function multiply(num1, num2) {
  */
 export function vote(age) {
   // TODO
+  if (typeof age !== "number") {
+    return "Age not valid.";
+  } else if (age < 18) {
+    return "You must be 18 or older to vote.";
+  } else if (age >= 18) {
+    return "Who would you like to vote for?";
+  }
 }
 
 /**
@@ -29,4 +36,9 @@ export function vote(age) {
  */
 export function concatenateStrings(str1, str2) {
   // TODO
+  if (str1 !== "string" || str2 !== "string") {
+    return str1 + str2;
+  } else {
+    console.log`undefined`;
+  }
 }
